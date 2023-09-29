d7ccde7fe9ba524847002c2f5a125edcaa5bfd4a
Igor Batista <105023521+IgorEBatista@users.noreply.github.com>
2023-09-29 00:33:07 -0300

    fim da le coisas

--- lab08/lab08a.s
+++ lab08/lab08a.s
@@ -28,6 +28,7 @@ write:
     ret
 
 setPixel:
+    ##Constrói a cor, e pinta o pixel designado
     # a0: pixel's x coordinate
     # a1: pixel's y coordinate
     # a2: concatenated pixel's colors: R|G|B|A
@@ -36,9 +37,18 @@ setPixel:
         # A2[15..8]: Blue
         # A2[7..0]: Alpha
     # a7: 2200 (syscall number)
-    # li a0, 100 # x coordinate = 100
-    # li a1, 200 # y coordinate = 200
-    # li a2, 0xFFFFFFFF # white pixel
+    # Constroi a cor
+    li t0, 0 # t0 = 0 zera um temporário
+    or t0, t0, a2 #adiciona a primeira parcela da cor
+    slli t0, t0, 8
+    or t0, t0, a2 #adiciona a segunda parcela da cor
+    slli t0, t0, 8
+    or t0, t0, a2 #adiciona a terceira parcela da cor
+    slli t0, t0, 8
+    mv  a2, t0 # a2 = t0 -- configura a impressão da cor
+    li t0, 0xFF # t0 = 0xFF
+    or a2, a2, t0
+    # Imprime
     li a7, 2200 # syscall setPixel (2200)
     ecall
     ret
@@ -64,52 +74,98 @@ openFile:
     ecall
     ret
 
-criaCor:
-    #a0:
 
 renderiza:
-    #a0: s11: file path
-    #a1: s10: largura da imagem
-    #a2: s9: altura da imagem
-    #s1: endereço dos bits
-    la s1, input # 
-    
-    mv  s11, a0 # s11 = a0
-    mv  s10, a1 # s10 = a1
-    mv  s9, a2 # s9 = a2
-    li t2, 0 # t2 = 0 contador de vetor
-    li t4, 0 # t4 = 0 contador de x
+    #a0 -> a3: endereco do byte atual  // Cord atual x
+    #a1 -> a4: largura da imagem // Cord atual y
+    #a2 -> a5: altura da imagem // Cor base pixel atual
+    #t0: apoio cria cor
+
+    mv  a3, a0 # a3 = a0
+    mv  a4, a1 # a4 = a1
+    mv  a5, a2 # a5 = a2
+
+    li a0, 0 # a0 = 0 contador de x
     li t5, 0 # t5 = 0 contador de y
     mv  t6, ra # t6 = ra -- salva endereço de retorno
+
     1:
-        mv  a0, s11 # a0 = s11
-        mv a2, s10 # a2 = s10
-        jal read  # jump to read and save position to ra
-        mv  s8, s1 # s8 = s1
         2:
-            lb t1, 0(s8) # 
-            # li t1, 0x000000ff # t1 = 0x000000ff
-            jal criaCor
+            lb a2, 0(a3) # carrega o byte da vez
             
-            mv  a0, t4 # a0 = t4 - coord atual x
             mv  a1, t5 # a1 = t5 - coord atual y
-            mv  a2, t1 # a2 = t1 - cor do pixel
             jal setPixel  # jump to setPixel and save position to ra
             
-            addi t4, t4, 1; # t4 = t4 + 1
-            addi s8, s8, 1; # s8 = s8 + 1
+            addi a0, a0, 1 # a0 = a0 + 1
+            addi a3, a3, 1 # a3 = a3 + 1 atualiza o endereço do byte da vez
 
-            bne t4, s10, 2b # if t4 != s10 then 2b    
-        li t4, 0 # t4 = 0 - reseta contador de x
-        addi t5, t5, 1; # t5 = t5 + 1
+            bne a0, s10, 2b # if a0 != s10 then 2b    
+        li a0, 0 # a0 = 0 - reseta contador de x
+        addi t5, t5, 1 # t5 = t5 + 1
         bne t5, s9, 1b # if t5 != s9 then 1b
     mv  ra, t6 # ra = t6 -- usa endereço de retorno
     ret
 
-main:
+leCoisas:
+    #a0: inicio do cabecalho -> inicio da imagem
+    #a4: byte da vez
+    #t0: valor acumulado
+    #t1: endereço para salvar os dados
+    #t2: fase do loop
+    li a1, 32 # a1 = 32 -- carrega o espaco para comparação
+    li a2, 10 # a1 = 32 -- carrega o \n para comparação (e o valor 10 para multiplicação)
+    li a3, 48 # a3 = 48 -- carrega o '0', para facilitar a conversão
+    li t0, 0 # t0 = 0 -- zera o valor acumulado
+    li t2, 0 # t2 = 0 -- zera a fase
 
+    addi a0, a0, 3 # a0 = a0 + 3 -- skipa o numero magico
+    
+    #leLargura_Altura:
+    2:
+        #Na primeira iteração do loop externo, le largura, depois le altura
+        la t1, largura # carrega o endereço para salvar a largura
+        beq t0, zero, 1f # if t0 == zero then 1f
+        addi t2, t2, 5 # t2 = t2 + 5
+        la t1, altura # carrega o endereço para salvar a largura
+        li t0, 0 # t0 = 0 -- zera o valor acumulado
+        beq t2, a2, 2f # if t2 == a2 then 2f
+        
+        1:
+            lb a4, 0(a0) # carrega o byte da vez
+            addi a0, a0, 1 # a0 = a0 + 1
+            beq a4, a1, 1f # if a4 == a1 then 1f -- Verifica se é o espaço
+            beq a4, a2, 1f # if a4 == a2 then 1f -- Verifica se é um \n
+            add a4, a4, a3 # a4 = a4 + a3 -- converte em numero
+            mul t0, t0, a2 # Multiplica o valor anterior por 10
+            add t0, t0, a4 # t0 = t0 + a4 -- adiciona o novo digito ao total
+            j 1b  # jump to 1b
+        1:
+        
+        sb t0, 0(t1) # salva a largura
+        j 2b  # jump to 2b
+    2:
+    addi a0, a0, 4 # a0 = a0 + 4 -- skipa o max value (255 por padrão)
+    ret
+
+main:
+    # j a
     jal openFile # jump to open and save position to ra
-    mv  s0, a0 # s0 = a0 - salva o file path
+    mv s0, a0 # s0 = a0 - salva o file path
+    
+    la a2, TAM # 
+    lw a2, 0(a2) # 
+    jal read  # jump to read and save position to ra -- lê o arquivo completo
+
+    la s0, input # carrega o inicio do cabeçalho
+    mv  a0, s0 # a0 = s0 - passa o endereço de inicio
+    jal leCoisas  # jump to leCoisas and save position to ra
+    
+    mv  s1, a0 # s1 = a0 -- salva o endereço de inicio da imagem em s1
+
+
+    
+    
+    
     #set coisas
     #Lê o numero magico
     li a2, 3 # a2 = 3
@@ -137,9 +193,8 @@ main:
     lw a1, 0(t1) # 
     jal setCanvasSize
     
-
     #Lê imagem
-    mv  a0, s0 # a0 = s0
+    mv a0, s0 # a0 = s0
     la t0, largura # 
     lw a1, 0(t0) # 
     la t1, altura # 
@@ -147,15 +202,19 @@ main:
     jal renderiza  # jump to renderiza and save position to ra
     
     j exit
-ret
+    ret
 
 .data
 
 input_file: .asciz "image.pgm" # não bota um /0 no final, tem que testar se dá certo
 
-altura: .word 9
-largura: .word 16
+TAM: .word 262.144
+_largura: .word 16
+_altura: .word 9
+
+# input: .word 0
 
 .bss
-
-input: .skip 200
+largura: .skip 4
+altura: .skip 4
+input: .skip 262.144
