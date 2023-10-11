31ac8e96ac148bf833341666188ea8c524337e40
Igor Batista <igorengbatista@gmail.com>
2023-10-11 18:01:08 -0300

    inicio do lab 11 projetando estratégia

---
+++ lab11/lab11.s
@@ -0,0 +1,14 @@
+
+
+
+get_coord:
+    #Coleta as coordenadas do gps e salva numa estrutura
+    #a0: endereço da estrutura
+
+calc_dist:
+    #Calcula a distância de separação com o centro do objetivo
+    #a0: endereço da estrutura --> distância de separação
+
+calc_ang:
+    #Calcula a distancia angular entre a orientação do carro e o centro do objetivo
+    #a0: endereço da estrutura --> valor
