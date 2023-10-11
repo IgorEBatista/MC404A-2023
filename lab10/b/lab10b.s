92b9ce9156a3bacd758ca4025c29abe4e538cd75
Igor Batista <igorengbatista@gmail.com>
2023-10-11 16:08:41 -0300

    loop infinito de gets

--- lab10/b/lab10b.s
+++ lab10/b/lab10b.s
@@ -183,33 +183,68 @@ itoa:
     ret
 
         
-.globl linked_list_search
-linked_list_search:
+.globl recursive_tree_search
+recursive_tree_search:
     #a0: endereço do primeiro nó -> valor de retorno
     #a1: valor buscado
-    #t0: endereço do nó atual
-    #t1: parcela 1 /total
-    #t2: parcela 2
+    #t0: endereço do nó da vez
+    #t1: valor do nó da vez
 
-    mv  t0, a0 # t0 = a0
-    li a0, 0 # a0 = 0 -- zera o indice retornado
-    1: #inicio do loop
-        lw t1, 0(t0) # carrega a primeira parcela
-        lw t2, 4(t0) # carrega a segunda parcela
-        add t1, t1, t2 # t1 = t1 + t2
-        beq t1, a1, 1f # if t1 == a1 then 1f -- verifica se é o valor procurado
+    addi sp, sp, -4  # Salva enrdereço de ra na pilha do programa
+    sw   ra, 0(sp)
+
+    #Verifica se é o valor indicado
+        lw t1, 0(a0) # 
+        bne a1, t1, 1f # if a1 != t1 then 1f
         
-        lw t0, 8(t0) # carrega o endereço do próximo nó
-        beq t0, zero, 2f # if t0 == zero then 2f
-        addi a0, a0, 1 # a0 = a0 + 1
-        j 1b  # jump to 1b
+        #Caso sim, retorna 1
+        li a0, 1 # a0 = 1
+        j 6f  # jump to 6f
         
-    1: #fim do loop
-    ret
+        1:
+        #Empilha os próximos endereços
+        addi sp, sp, -8  # Salva os endereços dos nós na pilha do programa
+        lw t0, 4(sp) # 
+        sw   t0, 4(sp)
+        lw t0, 8(sp) # 
+        sw   t0, 0(sp)
+        #Caso não, verifica se é o filho da direita
+            lw a0, 0(sp) # carrega o endereço do filho da direita 
+            addi sp, sp, 4
+            beq a0, zero, 2f # if a0 == zero then 2f -- se não tiver esse filho, segue em frente
+            jal recursive_tree_search  # jump to recursive_tree_search and save position to ra
+            
+            #Caso retorne 0, segue
+            beq a0, zero, 2f # if a0 == zero then 2f
+            
+            #Caso retorne diferente de zero, retorna o valor + 1
+            addi a0, a0, 1 # a0 = a0 + 1
+            addi sp, sp, 4 # desempilha o outro nó não utilizado
+            j 6f  # jump to 6f
+            
+        2:
+        #Caso não, verifica se é o filho da esquerda    
+            lw a0, 0(sp) # carrega o endereço do filho da esquerda 
+            addi sp, sp, 4
+            beq a0, zero, 3f # if a0 == zero then 3f -- se não tiver esse filho, segue em frente
+            jal recursive_tree_search  # jump to recursive_tree_search and save position to ra
+            
+            #Caso retorne 0, segue
+            beq a0, zero, 3f # if a0 == zero then 3f
+
+            #Caso retorne diferente de zero, retorna o valor + 1
+            addi a0, a0, 1 # a0 = a0 + 1
+            j 6f  # jump to 6f
+        
+        3:
+        #Em último caso, retorna 0
+        li a0, 0 # a0 = 0
+        
     
-    2:
-        li a0, -1 # a0 = -1
-        ret
+    6:
+    lw   ra, 0(sp)    # Recupera endereço de ra da pilha
+    addi sp, sp, 4
+    ret
         
 .globl exit
 exit:
