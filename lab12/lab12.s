2497a47e3dc738ddaf0dd6c594bee82c4eb4355d
Igor Batista <igorengbatista@gmail.com>
2023-10-30 13:07:21 -0300

    fim lab 12

--- lab12/lab12.s
+++ lab12/lab12.s
@@ -8,13 +8,14 @@ exit:
     ecall
 
 
-gets: ##ADAPTADA -- guarda informações direto na pilha
-    #a0: endereço base MIMO -> numero de byts lidos
+gets: 
+    #a0: endereço base MIMO -> endereço da string
+    #a1: endereço do buffer
     #t1: byte da vez
     #t2: '\n'
-    #t3 inicio da pilha
+    #t3: endereço da vez
     
-    mv  t3, sp # t3 = sp
+    mv  t3, a1 # t3 = a1  
     li t2, 10 # t2 = 10
     
     1:
@@ -25,21 +26,25 @@ gets: ##ADAPTADA -- guarda informações direto na pilha
             nop
             lb t1, 2(a0) # 
             bne t1, zero, 2b # if t1 != zero then 2b
+        2:
 
         lb t1, 3(a0) # carrega o byte lido 
-        addi sp, sp, -1 # sp = sp + -1 abre espaço na pilha
-        sb t1, 0(sp) # salva o byte lido na pilha
+        sb t1, 0(t3) # salva o byte lido no buffer
+        addi t3, t3, 1 # t3 = t3 + 1 avança o endereço
         beq t1, t2, 1f # if t1 == t2 then 1f -- continua até achar o \n
 
         j 1b  
     1:
     li t1, 0 # t1 = 0
-    sb t1, 0(sp) # 
-    sub a0, t3, sp # a0 = t3 - sp
+    addi t3, t3, -1 # t3 = t3 + -1
+    sb t1, 0(t3) # 
+    # sub a0, t3, a1 # a0 = t3 - a1
+    # addi a0, a0, -1 # a0 = a0 + -1 -- não contar o /n -> 0 como um byte lido
+    mv  a0, a1 # a0 = a1
     
     ret
 
-atoi: ##ADAPTADA -- recebe a string na pilha
+atoi:
     #a0: endereço do inicio da string -> inteiro retornado
     #a1: base numerica do valor (10)
     #t0: byte da vez
@@ -47,13 +52,13 @@ atoi: ##ADAPTADA -- recebe a string na pilha
     #t2: sinal
     #t3: endereço da string
 
-    #remove espaços
+    #ignora espaços
     mv  t3, a0 # t3 = a0
     li t1, ' ' # t1 = ' '
     1:
         lb t0, 0(t3) # carrega o primeiro byte 
         bne t0, t1, 1f # if t0 != t1 then 1f
-        addi t3, t3, -1 # t3 = t3 + -1
+        addi t3, t3, 1 # t3 = t3 + 1
         j 1b
     1:
 
@@ -71,7 +76,7 @@ atoi: ##ADAPTADA -- recebe a string na pilha
     
     1:
         lb t0, 0(t3) # carrega o byte da vez
-        addi t3, t3, -1 # t3 = t3 + -1 -- avança para o próximo na pilha
+        addi t3, t3, 1 # t3 = t3 + 1 -- avança para o próximo na string
         li t1, 0 # t1 = 0
         beq t0, t1, 1f # if t0 == t1 then 1f -- Verifica se é o final
         addi t0, t0, -48 # t0 = t0 + -48
@@ -89,34 +94,37 @@ itoa:
     #t0: valor da vez / constante comparacao / sinal
     #t1: contador de repetição
     #t2: marcador de sinal
-    #t3: 10
 
     #Salva o endereço da string
     mv  a3, a1 # a3 = a1
     
-    #verifica se a base é 10, caso for o sinal é relevante
+    #verifica se é negativo
+    li t2, 0 # t2 = 0
+    bge a0, zero, normal # if a0 >= zero then normal
+    
+    #verifica se a base é 10
     li t3, 10 # t3 = 10
     bne a2, t3, 1f # if a2 != t3 then 1f
-    
-    li t2, 0 # t2 = 0
-    bge a0, zero, 1f # if a0 >= zero then 1f
     
     li t0, '-' # t0 = '-'
     sb t0, 0(a1) # adiciona o - ao começo da string
     addi a1, a1, 1 # a1 = a1 + 1
     li t2, 1 # t2 = 1
     sub a0, zero, a0 # a0 = zero - a0 -- inverte o sinal de a0    
-
-    1:
+    j normal  # jump to normal
+    
+    1:  #Verifica se a base é 16
+    li t3, 16 # t3 = 16
+    bne a2, t3, 2f # if a2 != t3 then 2f
+    
     li t1, 0 # t1 = 0 -- zera o contador    
     li t0, '\n' # t0 = '\n'
     addi sp, sp, -1
     sb t0, 0(sp) # adiciona o \n ao inicio da pilha
-    
-    #coloca na pilha os bytes a serem impressos
-    1: #inicio do loop
-        rem t0, a0, a2 # acha o resto da divisão
 
+    li t3, 10 # t3 = 10
+    1:
+        andi t0, a0, 0b1111
         bge t0, t3, 2f # if t0 >= t3 then 2f
         addi t0, t0, 48 # t0 = t0 + 48 -- transforma em caractere
         j 3f
@@ -128,10 +136,41 @@ itoa:
         sb t0, 0(sp) # adiciona o caractere na pilha
         addi t1, t1, 1 # t1 = t1 + 1 atualiza contador de caracteres
 
-        div a0, a0, a2 #divide pela base
+        srli a0, a0, 4 #divide por 16        
         bne a0, zero, 1b # if a0 != zero then 1b
     1: #fim do loop
+    j continua  # jump to continua
+    
+    ### Fim do caso especial se base 16 negativa
+    
+    
+    
+    normal:
+    li t1, 0 # t1 = 0 -- zera o contador    
+    li t0, '\n' # t0 = '\n'
+    addi sp, sp, -1
+    sb t0, 0(sp) # adiciona o \n ao inicio da pilha
+    
+    li t3, 10 # t3 = 10
+    #coloca na pilha os bytes a serem impressos
+    1: #inicio do loop
+        rem t0, a0, a2 # acha o resto da divisão
+        bge t0, t3, 2f # if t0 >= t3 then 2f
+        addi t0, t0, 48 # t0 = t0 + 48 -- transforma em caractere
+        j 3f
+        2:
+        sub t0, t0, t3 # t0 = t0 - t3
+        addi t0, t0, 'A' # t0 = t0 + 'A' -- transoforma em caractere hexa
+        3:
+        addi sp, sp, -1
+        sb t0, 0(sp) # adiciona o caractere na pilha
+        addi t1, t1, 1 # t1 = t1 + 1 atualiza contador de caracteres
 
+        div a0, a0, a2 #divide pela base        
+        bne a0, zero, 1b # if a0 != zero then 1b
+    1: #fim do loop de caso normal
+
+    continua:
     mv  a0, t1 # a0 = t1
     addi a0, a0, 1 # soma 1 para contar o /n
     add a0, a0, t2 # a0 = a0 + t2 -- para contar a adição ou não do sinal de menos
@@ -165,6 +204,8 @@ puts: ##ADAPTADA -- byte a byte
 
     mv  t0, a0 # t0 = a0  -- inicia o endereço no inicio da string
     li t2, '\n' # t2 = '\n'
+    
+    ## Encontra o fim da string
     1:
         lb t1, 0(t0) # carrega o byte da vez 
         beq t1, zero, 1f # if t0 == zero then 1f
@@ -182,7 +223,7 @@ puts: ##ADAPTADA -- byte a byte
     li t2, 1 # t2 = 1
     1:
         lb t1, 0(t0) # carrega o byte da vez
-        sb t1, 2(a1) # salva para impressão
+        sb t1, 1(a1) # salva para impressão
         sb t2, 0(a1) # imprime 
         
         2: #Espera a escrita terminar
@@ -200,12 +241,185 @@ puts: ##ADAPTADA -- byte a byte
     sb t1, 0(t0) #    
     ret
 
-reverte:
-    #a0: endereço da string a ser invertida
+reverte: 
+    #a0: endereço da string a ser invertida -> endereço da string inicial
+    #t0: endereço atual
+    #t1: byte atual
+    #t2: contador de repetição
+
+    mv  t0, a0 # t0 = a0
+    mv  t2, zero # t2 = zero
+
+    1:
+        lb t1, 0(t0) # carega o byte
+        beq t1, zero, 1f # if t1 == zero then 1f -- verifica se a string acabou
+        addi sp, sp, -1 # sp = sp + -1 -- abre espaço na pilha
+        sb t1, 0(sp) # armazena o byte na pilha
+        addi t0, t0, 1 # t0 = t0 + 1 -- avança o endereço
+        addi t2, t2, 1 # t2 = t2 + 1 -- avança o contador
+        
+        j 1b  # jump to 1b
+    1:
+
+    mv  t0, a0 # t0 = a0 -- volta o endereço para o início da string
+
+    1:
+        lb t1, 0(sp) # carrega o byte da pilha
+        sb t1, 0(t0) # salva o byte na string
+        addi sp, sp, 1 # sp = sp + 1 -- remove da pilha
+        addi t0, t0, 1 # t0 = t0 + 1 -- avança o endereço
+        addi t2, t2, -1 # t2 = t2 + -1 -- avança o contador
+        bgt t2, zero, 1b # if t2 > zero then 1b
+    1:
+
+    ret
 
 main:
+    la s1, MIMO #
+    lw s1, 0(s1) # carrega o endereço base
 
+    mv  a0, s1 # a0 = s1
+    la a1, buffer # carrega o endereço do buffer
+    jal gets  # jump to gets and save position to ra
+    jal atoi  # jump to atoi and save position to ra
+
+    ##Identifica qual a opção
+    addi a0, a0, -1 # a0 = a0 + -1
+    beq a0, zero, opc1 # if a0 == zero then opc1
+    addi a0, a0, -1 # a0 = a0 + -1
+    beq a0, zero, opc2 # if a0 == zero then opc2
+    addi a0, a0, -1 # a0 = a0 + -1
+    beq a0, zero, opc3 # if a0 == zero then opc3
+    addi a0, a0, -1 # a0 = a0 + -1
+    beq a0, zero, opc4 # if a0 == zero then opc4
+    
+    
+
+    opc1:
+
+        mv  a0, s1 # a0 = s1
+        la a1, buffer # 
+        jal gets  # jump to gets and save position to ra
+        
+        mv  a1, s1 # a1 = s1
+        jal puts  # jump to puts and save position to ra
+
+        j exit
+
+    opc2:
+
+        mv  a0, s1 # a0 = s1
+        la a1, buffer # 
+        jal gets  # jump to gets and save position to ra
+        
+        jal reverte
+        
+        mv  a1, s1 # a1 = s1
+        jal puts  # jump to puts and save position to ra
+
+        j exit
+
+    opc3:
+
+        mv  a0, s1 # a0 = s1
+        la a1, buffer # 
+        jal gets  # jump to gets and save position to ra
+
+        jal atoi
+
+        la a1, buffer # 
+        li a2, 16 # a2 = 16
+        jal itoa  # jump to itoa and save position to ra
+        
+        mv  a1, s1 # a1 = s1
+        jal puts  # jump to puts and save position to ra
+        
+
+        j exit
+
+    opc4:
+
+        mv  a0, s1 # a0 = s1
+        la a1, buffer # 
+        jal gets  # jump to gets and save position to ra
+
+        mv  t0, a0 # t0 = a0
+        li t1, ' ' # t1 = ' '
+        la t3, numero1 # 
+        
+        1:
+            lb t2, 0(t0) # carrega o digito
+            beq t2, t1, 1f # if t2 == t1 then 1f
+            sb t2, 0(t3) # salva na string de apoio
+            addi t0, t0, 1 # t0 = t0 + 1
+            addi t3, t3, 1 # t3 = t3 + 1 
+            j 1b  # jump to 1b
+        1:    
+
+        addi t0, t0, 1 # t0 = t0 + 1
+        lb t2, 0(t0) # carrega a operação
+        
+        addi sp, sp, -4 # sp = sp + -4
+        sw t2, 0(sp) # salva a operação na pilha
+
+        ##identifica o segundo numero
+        addi t0, t0, 1 # t0 = t0 + 1
+        mv  a0, t0 # a0 = t0
+        jal atoi  # jump to atoi and save position to ra
+        la a1, numero2 # 
+        sw a0, 0(a1) # 
+
+        ## identifica o primeiro numero
+        la a0, numero1 # 
+        jal atoi  # jump to atoi and save position to ra
+
+        LW a1, numero2 # 
+        
+        ## identifica a operação
+        lw t1, 0(sp) # 
+        addi sp, sp, 4 # sp = sp + 4 -- traz a operação da pilha
+        li t0, '+' # t0 = '+'
+        bne t0, t1, 1f # if t0 != t1 then 1f
+        add a0, a0, a1 # a0 = a0 + a1
+        j 2f  # jump to 2f
+
+        1:
+        li t0, '-' # t0 = '-'
+        bne t0, t1, 1f # if t0 != t1 then 1f
+        sub a0, a0, a1 # a0 = a0 - a1
+        j 2f  # jump to 2f
+        
+        1:
+        li t0, '*' # t0 = '*'
+        bne t0, t1, 1f # if t0 != t1 then 1f
+        mul a0, a0, a1
+        j 2f  # jump to 2f
+        
+        1:
+        div a0, a0, a1
+
+        2:
+
+
+
+        la a1, buffer # 
+        li a2, 10 # a1 = 10
+        jal itoa  # jump to itoa and save position to ra
+        
+        mv  a1, s1 # a1 = s1
+        jal puts  # jump to puts and save position to ra
+        
+        j exit  # jump to exit
+        
 
 .data
 
-numero: .string "XXXXXXXXXXXXXXXXXXXX"
+MIMO: .word 0xFFFF0100
+
+.bss
+
+numero1: .skip 64
+
+numero2: .skip 4
+
+buffer: .skip 64
--- teste.s
+++ teste.s
@@ -1,74 +1,114 @@
-la a0, a # 
-la a1, b # 
-sw a1, 4(a0) # 
+itoa:
+    #a0: inteiro a ser convertido --> endereço da string de saida
+    #a1: endereço da string de saida
+    #a2: base numerica para conversão
+    #t0: valor da vez / constante comparacao / sinal
+    #t1: contador de repetição
+    #t2: marcador de sinal
+    #t3: 10 ou 16
+    #t4: 4294967296
 
-li a1, 5 # a1 = 12
+    li a0, 748 # a0 = -748
+    li a1, 0 # a1 = 0xFfffff000
+    li a2, 16 # a2 = 16
 
+    li t4, 0 # t4 = 0
 
-.globl recursive_tree_search
-recursive_tree_search:
-    #a0: endereço do primeiro nó -> valor de retorno
-    #a1: valor buscado
-    #t0: endereço do nó da vez
-    #t1: valor do nó da vez
-
-    addi sp, sp, -4  # Salva enrdereço de ra na pilha do programa
-    sw   ra, 0(sp)
-
-    #Verifica se é o valor indicado
-        lw t1, 0(a0) # 
-        bne a1, t1, 1f # if a1 != t1 then 1f
-        
-        #Caso sim, retorna 1
-        li a0, 1 # a0 = 1
-        j 6f  # jump to 6f
-        
-        1:
-        #Empilha os próximos endereços
-        addi sp, sp, -8  # Salva os endereços dos nós na pilha do programa
-        lw t0, 4(a0) # 
-        sw   t0, 4(sp)
-        lw t0, 8(a0) # 
-        sw   t0, 0(sp)
-        #Caso não, verifica se é o filho da direita
-            lw a0, 0(sp) # carrega o endereço do filho da direita 
-            addi sp, sp, 4
-            beq a0, zero, 2f # if a0 == zero then 2f -- se não tiver esse filho, segue em frente
-            jal recursive_tree_search  # jump to recursive_tree_search and save position to ra
-            
-            #Caso retorne 0, segue
-            beq a0, zero, 2f # if a0 == zero then 2f
-            
-            #Caso retorne diferente de zero, retorna o valor + 1
-            addi a0, a0, 1 # a0 = a0 + 1
-            addi sp, sp, 4 # desempilha o outro nó não utilizado
-            j 6f  # jump to 6f
-            
-        2:
-        #Caso não, verifica se é o filho da esquerda    
-            lw a0, 0(sp) # carrega o endereço do filho da esquerda 
-            addi sp, sp, 4
-            beq a0, zero, 3f # if a0 == zero then 3f -- se não tiver esse filho, segue em frente
-            jal recursive_tree_search  # jump to recursive_tree_search and save position to ra
-            
-            #Caso retorne 0, segue
-            beq a0, zero, 3f # if a0 == zero then 3f
-
-            #Caso retorne diferente de zero, retorna o valor + 1
-            addi a0, a0, 1 # a0 = a0 + 1
-            j 6f  # jump to 6f
-        
-        3:
-        #Em último caso, retorna 0
-        li a0, 0 # a0 = 0
-        
+    #Salva o endereço da string
+    mv  a3, a1 # a3 = a1
     
-    6:
-    lw   ra, 0(sp)    # Recupera endereço de ra da pilha
-    addi sp, sp, 4
-    ret
-        
+    #verifica se é negativo
+    li t2, 0 # t2 = 0
+    bge a0, zero, normal # if a0 >= zero then normal
+    
+    #verifica se a base é 10
+    li t3, 10 # t3 = 10
+    bne a2, t3, 1f # if a2 != t3 then 1f
+    
+    li t0, '-' # t0 = '-'
+    sb t0, 0(a1) # adiciona o - ao começo da string
+    addi a1, a1, 1 # a1 = a1 + 1
+    li t2, 1 # t2 = 1
+    sub a0, zero, a0 # a0 = zero - a0 -- inverte o sinal de a0    
+    j normal  # jump to normal
+    
+    1:  #Verifica se a base é 16
+    li t3, 16 # t3 = 16
+    bne a2, t3, 2f # if a2 != t3 then 2f
+    
+    li t1, 0 # t1 = 0 -- zera o contador    
+    li t0, '\n' # t0 = '\n'
+    addi sp, sp, -1
+    sb t0, 0(sp) # adiciona o \n ao inicio da pilha
 
-.data
-a: .word 12,0,0
-b: .word 5,0,0
+    li t3, 10 # t3 = 10
+    1:
+        andi t0, a0, 0b1111
+        bge t0, t3, 2f # if t0 >= t3 then 2f
+        addi t0, t0, 48 # t0 = t0 + 48 -- transforma em caractere
+        j 3f
+        2:
+        sub t0, t0, t3 # t0 = t0 - t3
+        addi t0, t0, 'A' # t0 = t0 + 'A' -- transoforma em caractere hexa
+        3:
+        addi sp, sp, -1
+        sb t0, 0(sp) # adiciona o caractere na pilha
+        addi t1, t1, 1 # t1 = t1 + 1 atualiza contador de caracteres
+
+        srli a0, a0, 4 #divide por 16        
+        bne a0, zero, 1b # if a0 != zero then 1b
+    1: #fim do loop
+    j continua  # jump to continua
+    
+    ### Fim do caso especial se base 16 negativa
+    
+    
+    
+    normal:
+    li t1, 0 # t1 = 0 -- zera o contador    
+    li t0, '\n' # t0 = '\n'
+    addi sp, sp, -1
+    sb t0, 0(sp) # adiciona o \n ao inicio da pilha
+    
+    li t3, 10 # t3 = 10
+    #coloca na pilha os bytes a serem impressos
+    1: #inicio do loop
+        rem t0, a0, a2 # acha o resto da divisão
+        bge t0, t3, 2f # if t0 >= t3 then 2f
+        addi t0, t0, 48 # t0 = t0 + 48 -- transforma em caractere
+        j 3f
+        2:
+        sub t0, t0, t3 # t0 = t0 - t3
+        addi t0, t0, 'A' # t0 = t0 + 'A' -- transoforma em caractere hexa
+        3:
+        addi sp, sp, -1
+        sb t0, 0(sp) # adiciona o caractere na pilha
+        addi t1, t1, 1 # t1 = t1 + 1 atualiza contador de caracteres
+
+        div a0, a0, a2 #divide pela base        
+        bne a0, zero, 1b # if a0 != zero then 1b
+    1: #fim do loop de caso normal
+
+    continua:
+    mv  a0, t1 # a0 = t1
+    addi a0, a0, 1 # soma 1 para contar o /n
+    add a0, a0, t2 # a0 = a0 + t2 -- para contar a adição ou não do sinal de menos
+
+    
+    #remove da pilha e coloca na string
+
+    1: #inicio do loop
+        lb t0, 0(sp) # remove o caractere da pilha 
+        addi sp, sp, 1
+        sb t0, 0(a1) # salva o byte na string
+        addi a1, a1, 1 # a1 = a1 + 1 -- avança para o próx endereço
+        addi t1, t1, -1 # t1 = t1 + 1 atualiza contador de caracteres
+
+        bne t1, zero, 1b # if t1 != zero then 1b
+    1: #fim do loop
+
+    lb t0, 0(sp) # remove o \n da pilha 
+    addi sp, sp, 1
+    sb t0, 0(a1) # salva o byte na string
+    mv  a0, a3 # a0 = a3
+    ret
