cea2a15d20b99bacb95041ebb86b22b9bb20245e
Igor Batista <igorengbatista@gmail.com>
2023-11-08 10:47:13 -0300

    fim lab 14

--- lab14/lab14.s
+++ lab14/lab14.s
@@ -1,5 +1,4 @@
 .text
-.align 4
 
 int_handler:
     ###### Syscall and Interrupts handler ######
@@ -46,10 +45,13 @@ int_handler:
     beq t0, a7, set_handbrake # if t0 == a7 then set_handbrake
     li t0, 12 # t0 = 12
     beq t0, a7, read_sensors # if t0 == a7 then read_sensors
-    li t0, a7 # t0 = a7
+    li t0, 15 # t0 = 15
     beq t0, a7, get_position # if t0 == a7 then get_position
     
     resolvido:
+    csrr t0, mepc  # load return address (address of the instruction that invoked the syscall)
+    addi t0, t0, 4 # adds 4 to the return address (to return after ecall) 
+    csrw mepc, t0  # stores the return address back on mepc
 
     # restaura o contexto
         lw x1, 0(sp) # Salva x1
@@ -86,10 +88,15 @@ int_handler:
         addi sp, sp, 124 # Desaloca espaço na pilha da ISR
         csrrw sp, mscratch, sp # Troca sp com mscratch novamente
   
-    csrr t0, mepc  # load return address (address of the instruction that invoked the syscall)
-    addi t0, t0, 4 # adds 4 to the return address (to return after ecall) 
-    csrw mepc, t0  # stores the return address back on mepc
     mret           # Recover remaining context (pc <- mepc)
+
+     # Ajustando MEPC para retornar de uma chamada de sistema
+ csrr a1, mepc # carrega endereço de retorno (endereço
+ # da instrução que invocou a syscall)
+ addi a1, a1, 4 # soma 4 no endereço de retorno
+ # (para retornar após a ecall)
+ csrw mepc, a1 # armazena endereço de retorno
+ # de volta no mepc
   
 
 set_engine_and_steering:
@@ -108,9 +115,9 @@ set_engine_and_steering:
     bgt a0, t0, 1f # if a0 > t0 then 1f
 
     #comanda o veículo
-    li t0, BASE_MIMO # t0 = BASE_MIMO
+    lw t0, BASE_MIMO # t0 = BASE_MIMO
     sb a0, 33(t0) # a posicao 0x21 controla o motor
-    sb a1, 32(s2) # a posicao 0x20 controla a direcao da roda
+    sb a1, 32(t0) # a posicao 0x20 controla a direcao da roda
 
     li a0, 0 # a0 = 0
     j resolvido  # jump to resolvido
@@ -123,7 +130,7 @@ set_handbrake:
     #a0: value stating if the hand brakes must be used.
     #a7: 11
 
-    li t0, BASE_MIMO # t0 = BASE_MIMO
+    lw t0, BASE_MIMO # t0 = BASE_MIMO
     sb a0, 34(t0) #  a posicao 0x22 controla o freio
     
     j resolvido  # jump to resolvido
@@ -133,7 +140,7 @@ read_sensors:
     #a0: address of an array with 256 elements that will store the values read by the luminosity sensor.
     #a7: 12
 
-    li t0, BASE_MIMO # t0 = BASE_MIMO
+    lw t0, BASE_MIMO # t0 = BASE_MIMO
     li t1, 1 # t1 = 1
     sb t1, 1(t0) #  a posicao 0x1 liga a cam
     1:
@@ -168,7 +175,7 @@ get_position:
     #a2: address of the variable that will store the value of z position.
     #a7: 15
 
-    li t0, BASE_MIMO # t0 = BASE_MIMO
+    lw t0, BASE_MIMO # t0 = BASE_MIMO
     li t1, 1 # t1 = 1
     sb t1, 0(t0) #  a posicao 0x1 liga o gps
     1:
@@ -193,14 +200,15 @@ _start:
     la t0, int_handler  # Load the address of the routine that will handle interrupts
     csrw mtvec, t0      # (and syscalls) on the register MTVEC to set the interrupt array.
 
-    .set BASE_MIMO 0xFFFF0100
+    .data 
+    BASE_MIMO: .word 0xFFFF0100
 
     .bss
 
-    fim_prog_stack: .skip 6400
+    fim_prog_stack: .skip 1024
     prog_stack:
 
-    fim_irs_stack: .skip 6400
+    fim_irs_stack: .skip 1024
     irs_stack:
 
     .text
@@ -212,7 +220,7 @@ _start:
 
 
 
-# Write here the code to change to user mode and call the function user_main (defined in another file).
+    # Write here the code to change to user mode and call the function user_main (defined in another file).
     csrr t1, mstatus # Update the mstatus.MPP
     li t2, ~0x1800 # field (bits 11 and 12)
     and t1, t1, t2 # with value 00 (U-mode)
@@ -222,54 +230,66 @@ _start:
     mret # PC <= MEPC; mode <= MPP;
 
 
-    # implement your control logic here, using only the defined syscalls
-motor:
-    #controla o motor do carro
+dirige:
+    #controla o motor do carro e a direção das rodas
     #a0: comando do motor (-1, 0, 1)
-    sb a0, 33(s2) # a posicao 0x21 controla o motor 
+    #a1: comando da roda (-127 ~ 127)
+    li a7, 10 # a7 = 10
+    ecall
     ret
 
 freia:
-    #aciona o freio de mão ( o motor deve estar desligado)
-    li t1, 1 # t1 = 1
-    sb t1, 34(s2) #  a posicao 0x22 controla o freio
-    ret
+    #controla o freio de mão
+    #a0: valor para o freio (1 = ligado ; 0 = desligado)
+    #a7: 11
 
-dir:
-    #controla a direção das rodas
-    #a0: comando da roda (-127 ~ 127)
-    sb a0, 32(s2) # a posicao 0x20 controla a direcao da roda 
+
+    li a0, 1 # a0 = 1
+    li a7, 11 # a7 = 11
+    ecall
     ret
 
 get_coord:
     #Coleta as coordenadas do gps e salva numa estrutura
-    #a0: endereço da estrutura
-    #a1: endereço base da memoria de acesso ao carro
+    #a0: endereço da estrutura -> endereço x
+    #a1: -> endereço y
+    #a2: -> endereço z
+    #t0: apoio de endereço e valores
     #t1: carregamento de valores
-    #t2: att dir
 
     addi sp, sp, -4  # Salva enrdereço de ra na pilha do programa
     sw   ra, 0(sp)
 
-    li t1, 1 # t1 = 1
-    sb t1, 0(a1) # aciona o gps
-    1:
-        nop
-        lb t1, 0(a1) # 
-        bne t1, zero, 1b # if t1 != zero then 1b
-
-    lw t1, 16(a1) # carrega a nova coord x
-
-    lw t2, 0(a0) # carrega a coord anterior x
-    sub t2, t1, t2 # t2 = t1 - t2 calcula a dir
-    sw t2, 8(a0) # salva a dir
-    sw t1, 0(a0) # salva a nova coord x
+    ##salva as coordenadas anteriores
+    lw t1, 0(a0) # carrega a coord anterior x
+    addi sp, sp, -8  # abre espaço na pilha do programa
+    sw t1, 0(sp) # salva a coord anterior de x na pilha do programa
+    lw t1, 4(a0) # carrega a coord anterior z
+    sw t1, 4(sp) # salva a coord anterior de z na pilha do programa
     
-    lw t1, 24(a1) # carrega a nova coord z
-    lw t2, 4(a0) # carrega a coord anterior z
-    sub t2, t1, t2 # t2 = t1 - t2 calcula a dir
-    sw t2, 12(a0) # salva a dir
-    sw t1, 4(a0) # salva a nova coord z
+    ## atualiza as coordenadas
+    mv  t0, a0 # t0 = a0
+    addi a0, t0, 0 # a0 = t0 + 0 -- passa para a0 o endereço de x na estrutura
+    addi a1, t0, 16 # a1 = t0 + 16 -- passa para a1 o endereço de y na estrutura
+    addi a2, t0, 4 # a2 = t0 + 4 -- passa para a2 o endereço de z na estrutura
+    li a7, 15 # a7 = 15
+    ecall
+    
+    #atualiza a direção
+    mv  a0, t0 # a0 = t0
+
+    lw t1, 0(a0) # carrega a nova coord x
+    lw t0, 0(sp) # carrega a coord antiga de x
+    sub t0, t1, t0 # t0 = t1 - t0 calcula a dir
+    sw t0, 8(a0) # salva a componente x da dir
+    
+    lw t1, 4(a0) # carrega a nova coord z
+    lw t0, 4(sp) # carrega a coord anterior z
+    sub t0, t1, t0 # t0 = t1 - t0 calcula a dir
+    sw t0, 12(a0) # salva a componente z da dir
+    
+    addi sp, sp, 8 #desempilha os valores
+    
 
     lw   ra, 0(sp)    # Recupera endereço de ra da pilha
     addi sp, sp, 4
@@ -334,11 +354,11 @@ calc_dir:
     addi sp, sp, 4
     
     bgt t0, zero, 1f # if t0 > zero then 1f
-    li a0, -63 # a0 = -127
+    li a0, -30 # a0 = -127
     ret
 
     1:
-    li a0, 63 # a0 = 127 #curva para direita
+    li a0, 30 # a0 = 127 #curva para direita
     ret
 
 root:
@@ -357,21 +377,22 @@ root:
     bnez t0, 1b # if t0 != 0  then  1b:
     ret
 
+# implement your control logic here, using only the defined syscalls
 .globl control_logic
 control_logic:
 #Inicializacao
     la s1, estrutura #  s1 será o endereço da estrutura
-    LW s2, base # s2 será o endereço base do acesso à memória
+    li s2, 1 # s2 = 1 -- s2 manterá o estado atual do motor
     la s3, alvo # s3 será o endereço do alvo
     # A padronização acima foi feita para diminuir o numero de acessos a memória, 
     # visando maior otimização de processamento do sistema embarcado
 
     mv  a0, s1 # a0 = s1
-    mv  a1, s2 # a1 = s2
     jal get_coord  # jump to get_coord and save position to ra
     
     li a0, 1 # a0 = 1
-    jal motor  # jump to motor and save position to ra
+    li a1, 0 # a1 = 0
+    jal dirige  # jump to dirige and save position to ra
     
     li a7, 5000 # a7 = 5000
     1:
@@ -380,7 +401,6 @@ control_logic:
         bne a7, zero, 1b # if a7 != zero then 1b
 
     mv  a0, s1 # a0 = s1
-    mv  a1, s2 # a1 = s2
     jal get_coord  # jump to get_coord and save position to ra
 
 #Conducao
@@ -388,51 +408,45 @@ control_logic:
         mv  a0, s1 # a0 = s1
         mv  a1, s3 # a1 = s3
         jal calc_dir  # jump to calc_dir and save position to ra
-        jal dir  # jump to dir and save position to ra
+        mv  a1, a0 # a1 = a0
+        mv  a0, s2 # a0 = s2
+        jal dirige  # jump to dirige and save position to ra
 
-        mv  a0, s1 # a0 = s1
-        mv  a1, s2 # a1 = s2
-        jal get_coord  # jump to get_coord and save position to ra
-        
         li a7, 1000 # a7 = 1000
         2:
             nop
             addi a7, a7, -1 # a7 = a7 + -1
             bne a7, zero, 2b # if a7 != zero then 2b
+        2:
 
-
-
+        mv  a0, s1 # a0 = s1
+        jal get_coord  # jump to get_coord and save position to ra
+        
         mv  a0, s1 # a0 = s1
         mv  a1, s3 # a1 = s3
         jal calc_dist  # jump to calc_dist and save position to ra
         
-        li t1, 30 # t1 = 30
+        li t1, 20 # t1 = 30
         sub a0, a0, t1 # a0 = a0 - t1
-        blt a0, zero, 1f # if a0 < zero then 1f
-        li t1, 30 # t1 = 30
+        bgt a0, zero, 3f # if a0 < zero then 3f
+        jal freia  # jump to freia and save position to ra
+        
+        3:
+        li t1, 60 # t1 = 30
         sub a0, a0, t1 # a0 = a0 - t1
         bgt a0, zero, 1b # if a0 > zero then 1b
-        li a0, 0 # a0 = 0
-        jal motor  # jump to motor and save position to ra
+        li s2, 0 # s2 = 0 - torna o estado atual do motor para desligado
 
         j 1b  # jump to 1b
         
     1:
-#Finalizacao
-    
-    jal freia  # jump to freia and save position to ra
-
-    li a7, 1000 # a7 = 1000
-        2:
-            nop
-            addi a7, a7, -1 # a7 = a7 + -1
-            bne a7, zero, 2b # if a7 != zero then 2b
 
 
-base: .word 0xFFFF0100
-
+.data
 #estrutura:
     # Coord x,z (int,int)
     # Vetor dir x,z (int,int)
+    # Dados y: coord y (int)
+estrutura: .word 0,0,0,0,0
 
-estrutura: .word 0,0,0,0
+alvo: .word 73, -14
